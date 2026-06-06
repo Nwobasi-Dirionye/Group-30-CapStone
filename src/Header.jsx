747911a34615b8d649bf6b7c37c419137377aac9
nwobasi dirionye diribeya <nwobasidirionye@gmail.com>
2026-06-06 16:19:05 +0100

    Updated header and hero section

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,12 +4,12 @@ import logo from "./assets/logo.png";
 const Header = () => {
   return (
     <div className='headbox'>
-      <div className="logo">
+      
         <img className="my-logo" src={logo} alt="logo" />
-    </div>
+
 </div>
     
-  )
+  );
 }
 
 export default Header;
